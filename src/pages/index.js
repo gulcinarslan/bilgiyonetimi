4c06e6e082e07e40cfed2e861f25759835f31b81
Gülçin Ofluoğlu <70818564+gulcinofluoglu@users.noreply.github.com>
2020-09-07 10:53:51 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,7 +61,7 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Atatürk Üniversitesi ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
@@ -74,7 +74,7 @@ function Home() {
                 styles.getStarted,
               )}
               to={useBaseUrl('docs/')}>
-              Get Started
+              Giriş
             </Link>
           </div>
         </div>
